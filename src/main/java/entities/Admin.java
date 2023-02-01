583996dff80a54657fdd7201db42a21845a85088
Asmaa <s12028417@stu.najah.edu>
2023-02-01 22:18:11 +0200

    entities

---
+++ src/main/java/entities/Admin.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Admin {
+    private String name;
+    private String password;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/entities/Product.java
@@ -0,0 +1,24 @@
+package entities;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Product {
+    private String category;
+    private String name;
+    private String description;
+    /*
+        private String picturePath = "";
+    BufferedImage picture;
+
+    {
+        try {
+            picture = ImageIO.read(new File(picturePath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    */
+}
