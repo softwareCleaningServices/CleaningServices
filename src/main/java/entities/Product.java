a7a60c794c4d13b88673ab0434b70cc82b10ad93
Asmaa <s12028417@stu.najah.edu>
2023-02-28 16:43:56 +0200

    edit package entity on product class

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -4,21 +4,80 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Product {
-    private String category;
     private String name;
-    private String description;
-    /*
-        private String picturePath = "";
+    private String pictureName;
+    private String dimension;
+    private String material;
+    private String specialTreatment;
+    private String picturePath;//TODO name from user then concatenation with path
     BufferedImage picture;
-
+    /*
     {
         try {
-            picture = ImageIO.read(new File(picturePath));
+            picture = ImageIO.read(getClass().getResource("\"resources/img/carpet.jpg\""));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    */
+    * */
+
+    public Product() {
+
+    }
+
+    public Product(String name, String pictureName, String dimension, String material, String specialTreatment) {
+        this.name = name;
+        this.pictureName = pictureName;
+        this.dimension = dimension;
+        this.material = material;
+        this.specialTreatment = specialTreatment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPictureName() {
+        return pictureName;
+    }
+
+    public void setPictureName(String pictureName) {
+        this.pictureName = pictureName;
+    }
+
+    public String getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(String dimension) {
+        this.dimension = dimension;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getSpecialTreatment() {
+        return specialTreatment;
+    }
+
+    public void setSpecialTreatment(String specialTreatment) {
+        this.specialTreatment = specialTreatment;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + pictureName + " " + dimension + " " + material + " " + specialTreatment ;
+    }
 }
