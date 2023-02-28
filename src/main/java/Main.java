44f4d716183d87cfbe236ad1bf595cb1ba103493
Asmaa <s12028417@stu.najah.edu>
2023-02-28 16:49:37 +0200

    main and new product picture for test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
+import entities.Product;
+
 public class Main {
     public static void main(String[]args){
         System.out.println("Hello This is for testing");
+        productFile pro= new productFile();
+        Product a=new Product("carpet","carpet","200*100","cloth","no");
+        Product b=new Product("a1","carpet","200*100","cloth","no");
+        Product c=new Product("a2","carpet","200*100","cloth","no");
+        pro.addProduct(a);
+        pro.addProduct(b);
+        pro.addProduct(c);
     }
 }
