1edd020cbc42b21e62fd66496a189fd80c8a0ee8
Asmaa <s12028417@stu.najah.edu>
2023-03-05 00:50:12 +0200

    change all things

--- src/test/java/recordProduct.java
+++ src/test/java/recordProduct.java
@@ -1,9 +1,36 @@
+import entities.Category;
 import entities.Customer;
 import entities.Product;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
 public class recordProduct {
     Product product;
+    Category category;
+    @Given("that I recorded a new {string}")
+    public void recordCategory(String string) {
+        category=Category.valueOf(string.toUpperCase());
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I insert the necessary {string} information")
+    public void insertInfo(String string) {
+        String name=null;
+        String pictureName=null;
+        String dimension=null;
+        String material=null;
+        String specialTreatment=null;
+        product=new Product(name,pictureName,dimension,material,specialTreatment,Category.valueOf(string.toUpperCase()));
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the {string} will recorded successfully")
+    public void recordSuccess(String string) {
+
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 }
--- src/test/resources/feature/productInformation.feature
+++ src/test/resources/feature/productInformation.feature
@@ -4,14 +4,14 @@ Feature:record full information for product
 #    When I insert the necessary product information.
 #    Then the product will recorded successfully
   Scenario: record with category carpet
-    Given that I recorded a new carpet
-    When I insert the necessary carpet information
-    Then the carpet will recorded successfully
+    Given that I recorded a new "carpet"
+    When I insert the necessary "carpet" information
+    Then the "carpet" will recorded successfully
   Scenario: record with category cover
-    Given that I recorded a new cover
-    When I insert the necessary cover information
-    Then the cover will recorded successfully
+    Given that I recorded a new "cover"
+    When I insert the necessary "cover" information
+    Then the "cover" will recorded successfully
   Scenario: record with category sofa
-    Given that I recorded a new sofa
-    When I insert the necessary sofa information
-    Then the sofa will recorded successfully
+    Given that I recorded a new "sofa"
+    When I insert the necessary "sofa" information
+    Then the "sofa" will recorded successfully
