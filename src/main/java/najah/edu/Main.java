1287119331ef76201657df7cab54280600a74490
ruba <ruba@gmail.com>
2023-03-06 18:57:52 +0200

    Main Class

--- src/main/java/najah/edu/Main.java
+++ src/main/java/najah/edu/Main.java
@@ -1,8 +1,40 @@
 package najah.edu;
-import java.util.Scanner;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Main {
+
+    public static int menu(){
+        System.out.println("Welcome to AR-Cleaning Services System");
+        Scanner in=new Scanner(System.in);
+        int option;
+        try {
+            System.out.println("If you want to login enter number 1");
+            System.out.println("If you are new customer enter number 2");
+             option = in.nextInt();
+
+        }
+        catch (InputMismatchException e){
+            System.out.println("please enter a valid number");
+           option= menu();
+        }
+        return option;
+    }
+
     public static void main(String[]args){
+    int option=menu();
+    if(option==1){
+        userLogin();
+    }
+    else {
+        RecordCustomer recordCustomer=new RecordCustomer();
+        recordCustomer.newCustomer();
+    }
+    }
+
+
+    public static void userLogin(){
+
         Scanner in=new Scanner(System.in);
         System.out.println("***************Welcome Back***************");
         while(true) {
@@ -10,15 +42,16 @@ public class Main {
             String email = in.nextLine();
             System.out.print("Enter your password ");
             String password = in.nextLine();
-            AdminLogin adminLogin = new AdminLogin();
-            adminLogin.setPassword(password);
-            adminLogin.setEmail(email);
-            if (adminLogin.isCorrectInfo()) {
-                adminLogin.adminOptions();
+            Login login=new Login();
+            login.setEmail(email);
+            login.setPassword(password);
+            if(login.isCorrectInfo()){
                 break;
-            } else {
-                System.out.println("Incorrect user name or password try again");
             }
+            else {
+                System.out.println("Incorrect user name or password, try again");
+            }
+
         }
     }
 }
