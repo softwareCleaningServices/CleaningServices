dd3bcd5f2c3f502930e6184d373ee130bba03e8a
ruba <ruba@gmail.com>
2023-03-06 18:49:33 +0200

    Record & track Order Feature

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,14 +1,48 @@
 package entities;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
 public class Product {
     private String category;
+
+    public Product() {
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     private String name;
     private String description;
+    private int coast;
+
+    public int getCoast() {
+        return coast;
+    }
+
+    public void setCoast(int coast) {
+        this.coast = coast;
+    }
+
+    public Product(String category, String name, String description) {
+        this.category = category;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "category=" + category +
+                ", name='" + name + '\'' +
+                ", description='" + description ;
+    }
     /*
         private String picturePath = "";
     BufferedImage picture;
