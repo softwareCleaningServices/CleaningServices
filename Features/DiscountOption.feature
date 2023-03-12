edcff072bcc57c0ea59fdf4d910042234ef99a03
Asmaa <s12028417@stu.najah.edu>
2023-03-13 00:56:05 +0200

    Successfully complete all test cases with the discount option

--- Features/DiscountOption.feature
+++ Features/DiscountOption.feature
@@ -1,24 +1,18 @@
 #we have four scenario: 500 150 100 noDiscount
 Feature:Discount on order
-  Scenario Outline: discount on order five hundred or more
+  Scenario: discount on order five hundred or more
     Given that i add a new order
     When the total cost of the order more than or equal five hundred
-    Then the order have a discount twenty percent <total>
-    Examples:
-      | total |
-      | 600.0 |
-
-#  Scenario: discount on order one hundred fifty and less than five hundred
-#    Given that i add a new order
-#    When the total cost of the order more than or equal one hundred fifty
-#    And the total cost of the order less than five hundred
-#    Then the order have a discount fifteen percent
-#  Scenario: discount on order one hundred and less than one hundred fifty
-#    Given that i add a new order
-#    When the total cost of the order more than or equal one hundred
-#    And the total cost of the order less than one hundred fifty
-#    Then the order have a discount seven percent
-#  Scenario: No discount on order less than one hundred
-#    Given that i add a new order
-#    When the total cost of the order less than one hundred
-#    Then the order haven't discount
+    Then the order have a discount twenty percent
+  Scenario: discount on order one hundred fifty and less than five hundred
+    Given that i add a new order
+    When the total cost of the order more than or equal one hundred fifty and less than five hundred
+    Then the order have a discount fifteen percent
+  Scenario: discount on order one hundred and less than one hundred fifty
+    Given that i add a new order
+    When the total cost of the order more than or equal one hundred and less than one hundred fifty
+    Then the order have a discount seven percent
+  Scenario: No discount on order less than one hundred
+    Given that i add a new order
+    When the total cost of the order less than one hundred
+    Then the order haven't discount
--- src/test/java/test/classes/DiscountOptionTest.java
+++ src/test/java/test/classes/DiscountOptionTest.java
@@ -1,5 +1,8 @@
 package test.classes;
+import entities.Category;
 import entities.Product;
+import entities.ProductFile;
+import entities.SizeOfCover;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -10,9 +13,10 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DiscountOptionTest {
     Order order;
-    ArrayList<Product> product;
+    Product product1;
+    ArrayList <Product> product=new ArrayList<Product>();
     double discountFifty ;
-    double discountOneAndFifteen ;
+    double discountOneHunFifty;
     double discountOneHun ;
     double noDiscount ;
     @Given("that i add a new order")
@@ -21,50 +25,86 @@ public class DiscountOptionTest {
     }
     @When("the total cost of the order more than or equal five hundred")
     public void theTotalCostOfTheOrderMoreThanOrEqualFiveHundred() {
-        discountFifty= order.getTotal() >= 500?order.getTotal()-(order.getTotal()*15/100):order.getTotal();
+        product1=Product
+                .builder()
+                .setName("name")
+                .setPictureName("picName")
+                .setCover(SizeOfCover.KING)
+                .setMaterial("material")
+                .setSpecialTreatment("special")
+                .setCategory(Category.COVER)
+                .setCost(600.0)
+                .build();
+        product.add(product1);
+        order.setProducts(product);
+        discountFifty= order.getTotal() >= 500?order.getTotal()-(order.getTotal()*20/100):order.getTotal();
     }
-    @Then("the order have a discount twenty percent {double}")
-    public void theOrderHaveADiscountTwentyPercentTotal(Double double1) {
-        double1=double1-(double1*0.20);
-        assertEquals(double1,discountOneAndFifteen);
+    @Then("the order have a discount twenty percent")
+    public void theOrderHaveADiscountTwentyPercentTotal() {
+        assertEquals(ProductFile.totalAfterDiscount(order),discountFifty);
     }
-//    @When("the total cost of the order more than or equal one hundred fifty")
-//    public void theTotalCostOfTheOrderMoreThanOrEqualOneHundredFifty() {
-//        if (order.getTotal()>=150){
-//
-//        }
-//    }
-//    @And("the total cost of the order less than five hundred")
-//    public void theTotalCostOfTheOrderLessThanFiveHundred() {
-//        if (order.getTotal()<=500){
-//
-//        }
-//    }
-//    @Then("the order have a discount fifteen percent")
-//    public void theOrderHaveADiscountFifteenPercent() {
-//    }
-//    @When("the total cost of the order more than or equal one hundred")
-//    public void theTotalCostOfTheOrderMoreThanOrEqualOneHundred() {
-//        if (order.getTotal()>=100){
-//
-//        }
-//    }
-//    @And("the total cost of the order less than one hundred fifty")
-//    public void theTotalCostOfTheOrderLessThanOneHundredFifty() {
-//        if (order.getTotal()<=150){
-//
-//        }
-//    }
-//    @Then("the order have a discount seven percent")
-//    public void theOrderHaveADiscountSevenPercent() {
-//    }
-//    @When("the total cost of the order less than one hundred")
-//    public void theTotalCostOfTheOrderLessThanOneHundred() {
-//        if (order.getTotal()<=100) {
-//
-//        }
-//    }
-//    @Then("the order haven't discount")
-//    public void theOrderHavenTDiscount() {
-//    }
+    @When("the total cost of the order more than or equal one hundred fifty and less than five hundred")
+    public void theTotalCostOfTheOrderBetweenOneHundredFiftyAndFiveHundred() {
+        product1=Product
+                .builder()
+                .setName("name")
+                .setPictureName("picName")
+                .setCover(SizeOfCover.KING)
+                .setMaterial("material")
+                .setSpecialTreatment("special")
+                .setCategory(Category.COVER)
+                .setCost(170.0)
+                .build();
+        product.add(product1);
+        order.setProducts(product);
+        if (order.getTotal()>=150 && order.getTotal()<500){
+            discountOneHunFifty=order.getTotal()-(order.getTotal()*0.15);
+        }
+    }
+    @Then("the order have a discount fifteen percent")
+    public void theOrderHaveADiscountFifteenPercent() {
+        assertEquals(ProductFile.totalAfterDiscount(order), discountOneHunFifty);
+    }
+    @When("the total cost of the order more than or equal one hundred and less than one hundred fifty")
+    public void theTotalCostOfTheOrderBetweenOneHundredAndOneHundredFifty() {
+        product1=Product
+                .builder()
+                .setName("name")
+                .setPictureName("picName")
+                .setCover(SizeOfCover.KING)
+                .setMaterial("material")
+                .setSpecialTreatment("special")
+                .setCategory(Category.COVER)
+                .setCost(120.0)
+                .build();
+        product.add(product1);
+        order.setProducts(product);
+        if (order.getTotal()>=100 && order.getTotal()<150){
+            discountOneHun=order.getTotal()-(order.getTotal()*0.07);
+        }
+    }
+    @Then("the order have a discount seven percent")
+    public void theOrderHaveADiscountSevenPercent() {
+        assertEquals(ProductFile.totalAfterDiscount(order),discountOneHun);
+    }
+    @When("the total cost of the order less than one hundred")
+    public void theTotalCostOfTheOrderLessThanOneHundred() {
+        product1=Product
+                .builder()
+                .setName("name")
+                .setPictureName("picName")
+                .setCover(SizeOfCover.KING)
+                .setMaterial("material")
+                .setSpecialTreatment("special")
+                .setCategory(Category.COVER)
+                .setCost(50.0)
+                .build();
+        product.add(product1);
+        order.setProducts(product);
+        noDiscount=order.getTotal();
+    }
+    @Then("the order haven't discount")
+    public void theOrderHavenTDiscount() {
+        assertEquals(ProductFile.totalAfterDiscount(order),noDiscount);
+    }
 }
