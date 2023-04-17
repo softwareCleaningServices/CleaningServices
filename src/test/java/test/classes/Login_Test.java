ae2b5f209b37e4922dc2a41ff330f8a340ef22a3
ruba <ruba@gmail.com>
2023-04-18 01:16:47 +0300

    update Login feature

--- src/test/java/test/classes/Login_Test.java
+++ src/test/java/test/classes/Login_Test.java
@@ -9,10 +9,11 @@ import static org.junit.Assert.assertEquals;
 
 public class Login_Test {
 boolean logged;
-Login login=new Login();
+Login login;
 
     @Given("that the user is not logged in")
     public void that_the_user_is_not_logged_in() {
+        login=new Login();
         logged=false;
     }
 
@@ -35,5 +36,18 @@ Login login=new Login();
     public void the_message_will_be_display(String msg) {
         assertEquals(login.msg(), msg);
         System.out.println(msg);
+    }
+    @Then("the user move to the {string}")
+    public void the_user_move_to_the(String page) {
+
+       if(page.equals("adminPage")){
+         login.adminLogin();
+        } else if (page.equals("customerPage")) {
+          login.customerLogin();
+       }
+       else {
+           login.loginPage();
+       }
+
     }
 }
