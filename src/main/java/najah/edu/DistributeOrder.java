89d69fea5fd36002bb2625b8468b9a2a76e88a63
ruba <ruba@gmail.com>
2023-04-25 19:55:00 +0300

    last update distribute order class

--- src/main/java/najah/edu/DistributeOrder.java
+++ src/main/java/najah/edu/DistributeOrder.java
@@ -1,63 +1,47 @@
 package najah.edu;
 
 import entities.Data;
-import entities.Product;
 import entities.Worker;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class DistributeOrder {
-    static Logger logger = Logger.getLogger(DistributeOrder.class.getName());
     public DistributeOrder(){}
 
-    public static Worker getWorker(Product product){
-        List<Worker>workers=Data.getWorkers();
-        int min=workers.get(0).getNumOfProd();
-        Worker worker=workers.get(0);
-        int ind=0;
-        for (Worker worker1:workers){
-            if(worker1.getCategory().equals(product.getCategory())){
-
-                if(min>worker1.getNumOfProd() ) {
-                    worker = worker1;
-                    ind=workers.indexOf(worker1);
-                    min=worker1.getNumOfProd();
-                }
+public static Worker getWorker(Order order){
+        List<Order>orders=Data.getOrders();
+        orders.add(order);
+        Worker worker=new Worker();
+        List<Integer> ids=distributeOrder(orders,Data.getWorkers());
+        for (Integer id:ids){
+            worker=Data.getWorkerById(id);
+            if (order.getStatus().equals("waiting")){
+                order.setWorker(worker);
             }
         }
-        workers.get(ind).setNumOfProd();
-        updateWorkerFile(workers);
         return worker;
-    }
-
-    public static void updateWorkerFile(List<Worker>workers){
-        try (
-            RandomAccessFile raf = new RandomAccessFile("src/main/resources/Back/Worker.txt", "rw");
-            BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/Back/Worker.txt"))){
-                writer.write("");
-                writer.flush();
-            for (Worker worker : workers) {
-                raf.seek(raf.length());
-                raf.write(worker.toString().getBytes());
+}
+    public static List<Integer> distributeOrder(List<Order> orders, List<Worker> workers) {
+        List<Integer>workersId=new ArrayList<>();
+        int numOrders=orders.size();
+        int numWorkers=workers.size();
+        if (numWorkers == 0)
+            return workersId;
+        else if (numOrders == 0)
+            return workersId;
+        else {
+            int ind=0;
+            for (Order order:orders){
+                order.setWorker(workers.get(ind));
+                workersId.add(workers.get(ind).getId());
+                if(ind == workers.size()-1){
+                    ind=0;
+                }
+                else ind++;
             }
+
         }
-        catch(Exception e){
-            logger.info("Error");
-        }
-    }
-    public static int waitingOrdersForWorker(Worker worker){
-        int count=0;
-        for (Order order:Data.getOrders()){
-           for (Product product:order.getProducts()){
-               if(product.getWorkerId()==worker.getId() && order.getStatus().equalsIgnoreCase("waiting")){
-                   count++;
-               }
-           }
-        }
-        return count;
+        return workersId;
     }
 }
